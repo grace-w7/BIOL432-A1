3b4f3664d04241018ce26f1f707b4a9b032daa99
grace-w7 <nive54@gmail.com>
2025-01-07 14:54:32 -0500

    dataGenerato.R full script (fixed)

--- dataGenerato.R
+++ dataGenerato.R
@@ -1,10 +1,19 @@
 ## dataGenerato.R ##
 
-species_vec <- c(1:100)
-species_vec
+species_names <- c("A. gestroi", "A. klugii", "P. laevis", "T. biocellata", "P. scaber")
+species_vec <- sample(species_names, 100, replace = TRUE)
 
-limbw_vec
+limb_width <- abs(rnorm(100, mean = 4, sd = 1))
+limb_length <- abs(rnorm(100, mean = 9, sd = 2))
 
-limbl_vec
+observer_names <- c("McTaggart", "Zeno", "Socrates") 
+observer <- sample(observer_names, 100, replace = TRUE)
 
-  
+library(tibble)
+dat <- tibble("Species" = species_vec, "Limb Width (mm)" = limb_width, 
+              "Limb Length (mm)" = limb_length, "Observer" = observer)
+head(dat)
+tail(dat)
+
+write.csv(dat, "measurements.csv", row.names = FALSE)
+
