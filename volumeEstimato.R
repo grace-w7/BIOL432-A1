59f3489f69a49c94b3895f6996c4c8cdd4253fca
grace-w7 <nive54@gmail.com>
2025-01-07 22:43:15 -0500

    Fixed typo in code

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -12,8 +12,9 @@ meas_dat <- meas_dat %>%
   mutate(Volume_mm3 = pi * (Limb_Width_mm / 2)^2 * Limb_Length_mm)
 
 #Overwrite old csv file with new data
-write.csv(dat, "measurements.csv", row.names = FALSE)
+write.csv(meas_dat, "measurements.csv", row.names = FALSE)
 
 #Check changed data frame
 head(meas_dat)
 tail(meas_dat)
+
